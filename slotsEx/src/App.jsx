d5911fd78d0f0c6aa38261c76b51918f28f74fa8
ramchandragawade <raajgawade39@gmail.com>
2024-01-07 01:38:31 +0530

    reset

--- slotsEx/src/App.jsx
+++ slotsEx/src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css'
-import Slots from './Slots'
 
 function App() {
 
   return (
     <div>
-      <Slots val1="🍒" val2="🍒" val3="🍒"/>
-      <Slots val1="🍒" val2="🍌" val3="🍌"/>
-      <Slots val1="🍒" val2="🍌" val3="⭐"/>
+      
     </div>
   )
 }
