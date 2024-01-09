213bf26a9c059994b9fc4481aadf257ece3c08c5
Ramchandra Gawade <ramchandra.gawade@northgateps.com>
2024-01-10 02:38:54 +0530

    property list data

--- slotsEx/src/App.jsx
+++ slotsEx/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import PropertyList from './PropertyList';
 import ShoppingList from './ShoppingList'
 
 const data = [
@@ -9,10 +10,22 @@ const data = [
   {item: 'cookie', qty: 10, completed: false},
   {item: 'maggi', qty: 12, completed: false}
 ]
+
+
+const properties = [
+  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+];
+
 function App() {
 
   return (
-    <ShoppingList items = {data}/>
+    // <ShoppingList items = {data}/>
+    <PropertyList properties = {properties}/>
   )
 }
 
