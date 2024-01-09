7b9aa7ca96b3ecc50a456de9a5ceab93715ef96d
Ramchandra Gawade <ramchandra.gawade@northgateps.com>
2024-01-10 02:56:47 +0530

    prop types

--- slotsEx/src/PropertyItem.jsx
+++ slotsEx/src/PropertyItem.jsx
@@ -1,7 +1,16 @@
-export default function PropertyItem({name, rating, price}) {
+import PropTypes from 'prop-types';
+function PropertyItem({name, rating, price}) {
     return <div style={{border: "1px solid red", margin:"0px px"}}>
         <h2>{name}</h2>
         <h3>${price}/night</h3>
         <h4>{rating}⭐</h4>
     </div>
 }
+
+PropertyItem.propTypes = {
+    name: PropTypes.string,
+    rating: PropTypes.number,
+    price: PropTypes.bool
+}
+
+export default PropertyItem;
--- slotsEx/src/PropertyList.jsx
+++ slotsEx/src/PropertyList.jsx
@@ -1,7 +1,13 @@
 import PropertyItem from "./PropertyItem";
-
-export default function PropertyList ({properties}) {
+import PropTypes from 'prop-types';
+function PropertyList ({properties}) {
     return <ul style={{display:"flex"}}>
         {properties.map((itm)=>(<PropertyItem key={itm.id} {...itm}/>))}
     </ul>
 }
+
+PropertyList.propTypes = {
+    properties: PropTypes.array
+}
+
+export default PropertyList;
