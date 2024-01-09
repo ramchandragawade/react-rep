f2692647399cdbbd1bec8adacef79007e92f84ef
Ramchandra Gawade <ramchandra.gawade@northgateps.com>
2024-01-10 02:41:43 +0530

    meh

--- slotsEx/src/App.jsx
+++ slotsEx/src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
 import PropertyList from './PropertyList';
-import ShoppingList from './ShoppingList'
+// import ShoppingList from './ShoppingList'
 
-const data = [
-  {item: 'egss', qty: 12, completed: false},
-  {item: 'bread', qty: 1, completed: false},
-  {item: 'lemons', qty: 6, completed: false},
-  {item: 'milk', qty: 2, completed: true},
-  {item: 'cookie', qty: 10, completed: false},
-  {item: 'maggi', qty: 12, completed: false}
-]
+// const data = [
+//   {item: 'egss', qty: 12, completed: false},
+//   {item: 'bread', qty: 1, completed: false},
+//   {item: 'lemons', qty: 6, completed: false},
+//   {item: 'milk', qty: 2, completed: true},
+//   {item: 'cookie', qty: 10, completed: false},
+//   {item: 'maggi', qty: 12, completed: false}
+// ]
 
 
 const properties = [
