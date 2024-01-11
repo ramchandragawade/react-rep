73d62e4cc2eb0c4c51f18e1595c9b21182c2eccb
ramchandragawade <raajgawade39@gmail.com>
2024-01-12 00:10:46 +0530

    toggle cnt

--- slotsEx/src/ToggleCounter.jsx
+++ slotsEx/src/ToggleCounter.jsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 function ToggleCounter() {
     const [isHappy, setIsHappy]  = useState(true);
     const [cnt, setCnt] = useState(0);
+    console.log('Component executed:',isHappy,cnt);
     const onToggle = () => {
         setIsHappy(!isHappy);
         setCnt(cnt+1);
+        console.log('set is called:',isHappy,cnt);
     };
     return <div>
         <button className="Toggler" onClick={onToggle}> {isHappy === true ? "😊" : "☹️"} </button>
