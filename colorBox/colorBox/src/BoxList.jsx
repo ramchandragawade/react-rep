6d47a381c25dfb2ccd25b2cd9a4fca14e1431dd5
ramchandragawade <raajgawade39@gmail.com>
2024-01-12 00:51:43 +0530

    create array of 25

--- colorBox/colorBox/src/BoxList.jsx
+++ colorBox/colorBox/src/BoxList.jsx
@@ -2,7 +2,7 @@ import Box from "./Box";
 import "./BoxList.css"
 export default function BoxList () {
     return <div className="BoxList">
-        {Array.apply(0, Array(25)).map(function (x, i) {
+        {Array(25).fill(null).map(function (x, i) {
             return <Box key={i} />;
         })}
       </div>
