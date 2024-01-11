8514add6cacc32bf6b21fa8d82b85ece5d820fdc
ramchandragawade <raajgawade39@gmail.com>
2024-01-12 01:34:44 +0530

    spread & create new obj to re render

---
+++ state-demo/src/ScoreKeeper.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function ScoreKeeper() {
+    const [scores, setScores] = useState({p1:0, p2:0});
+    const updateP1 = () => {
+        const newScore = {...scores, p1:scores.p1 + 1};
+        setScores(newScore);
+    }
+    const updateP2 = () => {
+        const newScore = {...scores, p2:scores.p + 1};
+        setScores(newScore);
+    }
+    console.log();
+    return (
+        <div>
+            <p>Player 1: {scores.p1}</p>
+            <p>Player 2: {scores.p2}</p>
+            <button onClick={updateP1}>+1 Player 1</button>
+            <button onClick={updateP2}>+1 Player 2</button>
+        </div>
+    )
+}
+
+export default ScoreKeeper;
