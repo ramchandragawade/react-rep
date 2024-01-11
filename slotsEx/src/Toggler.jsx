26ab533ba6abc864e41382fafd96a5221dc198b1
ramchandragawade <raajgawade39@gmail.com>
2024-01-11 23:54:18 +0530

    updt

--- slotsEx/src/Toggler.jsx
+++ slotsEx/src/Toggler.jsx
@@ -3,6 +3,6 @@ import { useState } from "react";
 function Toggler() {
     const[isHappy, setIsHappy]  = useState(true);
     const onToggle = () => setIsHappy(!isHappy);
-    return <p className="Toggler" onClick={onToggle}>{ isHappy === true? "😊" : "☹️" }</p>
+    return <p className="Toggler" onClick={onToggle}> {isHappy === true ? "😊" : "☹️"} </p>
 }
 export default Toggler;
