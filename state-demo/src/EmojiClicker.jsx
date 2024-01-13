810fa2b658b6aa37b4b28dc949832b83eb14cfd6
ramchandragawade <raajgawade39@gmail.com>
2024-01-13 16:35:44 +0530

    update all in set

--- state-demo/src/EmojiClicker.jsx
+++ state-demo/src/EmojiClicker.jsx
@@ -10,9 +10,13 @@ export default function EmojiClicker () {
     const removeItem = (id) => {
         setEmojis(e=>e.filter(e=>e.id !== id));
     }
+    const showAllHearts = () =>{
+        setEmojis(e=>e.map(i=>({...i,emoji: "💖"})))
+    }
     return <div>
         {emojis.map((itm)=>(<span key={itm.id} onClick={()=>removeItem(itm.id)} style={{fontSize: "4rem", cursor:"pointer"}}>{itm.emoji}</span>))}
         <br />
         <button onClick={addEmoji}>Add Emoji</button>
+        <button onClick={showAllHearts}>Make them all hearts</button>
     </div>
 }
