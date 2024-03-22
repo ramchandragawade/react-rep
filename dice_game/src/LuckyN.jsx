0676b583d08f2a38879c4c1a91ec2a3c484ab23a
ramchandragawade <raajgawade39@gmail.com>
2024-03-23 03:21:15 +0530

    dice game init

---
+++ dice_game/src/LuckyN.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import {getRolls, sum} from "./utils";
+import Dice from "./Dice";
+
+function LuckyN ({numDice=2, goal=7, color}){
+    const [dice, setDice] = useState(getRolls(numDice));
+    const isWinner = sum(dice) === goal;
+    const roll = ()=> setDice(getRolls(numDice));
+    return <main className="LuckyN">
+        <h1>Lucky {goal} {isWinner && "You Win!!"}</h1>
+        <Dice dice={dice} color={color}/>
+        <button onClick={roll}>Re-Roll Dice</button>
+    </main>
+}
+
+export default LuckyN;
