0c401ef06dd5442696a90b0277b2b1c54529b58b
ramchandragawade <raajgawade39@gmail.com>
2024-03-24 00:37:09 +0530

    refactor

--- todoList/src/TodoList.jsx
+++ todoList/src/TodoList.jsx
@@ -3,40 +3,35 @@ import { List } from "@mui/material";
 import { useState } from "react";
 import TodoItem from "./TodoItem";
 
-export default function TodoList(){
+export default function TodoList() {
     const initialTodos = [
-        {id: uuid(), text:"Walk the dog", completed: false},
-        {id: uuid(), text:"Walk the cat", completed: true},
-        {id: uuid(), text:"Buy milk", completed: true},
-        {id: uuid(), text:"Get bread", completed: false}
+        { id: uuid(), text: "Walk the dog", completed: false },
+        { id: uuid(), text: "Walk the cat", completed: true },
+        { id: uuid(), text: "Buy milk", completed: true },
+        { id: uuid(), text: "Get bread", completed: false }
     ];
 
     const updateCompleted = (id) => {
-        setTodos(curr=>{
-            return [...curr.map(itm=>{
-                if(itm.id===id){
-                    itm.completed = !itm.completed;
-                }
-                return itm;
-            })];
+        setTodos(curr => {
+            return curr.map(itm => itm.id !== id ? itm : { ...itm, completed: !itm.completed });
         });
     };
 
     const removeTodo = (id) => {
-        setTodos(curr=>{
-            return curr.filter(itm=>itm.id!==id);
+        setTodos(curr => {
+            return curr.filter(itm => itm.id !== id);
         });
     }
-    
+
     const [todos, setTodos] = useState(initialTodos);
     return (
         <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
             {
                 todos.map((item) => <TodoItem
-                        item={item} 
-                        updateCompleted={()=>{updateCompleted(item.id)}}
-                        key={item.id} removeTodo={()=>{removeTodo(item.id)}}
-                    />)
+                    item={item}
+                    updateCompleted={() => { updateCompleted(item.id) }}
+                    key={item.id} removeTodo={() => { removeTodo(item.id) }}
+                />)
             }
         </List>
     )
