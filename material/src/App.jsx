c1e97060dcc7ef0e9abeeb0c0b8730759d8bc6ec
ramchandragawade <raajgawade39@gmail.com>
2024-03-23 21:48:11 +0530

    slider & textfield

--- material/src/App.jsx
+++ material/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import BasicMat from './BasicMat'
+import FormDemo from './FormDemo'
 import RatingDemo from './RatingDemo'
 function App() {
 
   return (
     <>
       <BasicMat />
-
       <RatingDemo/>
+      <FormDemo/>
     </>
   )
 }
---
+++ material/src/FormDemo.jsx
@@ -0,0 +1,28 @@
+import { Slider, Stack, TextField } from "@mui/material";
+import { VolumeDown, VolumeUp } from "@mui/icons-material";
+import { useState } from "react";
+export default function FormDemo() {
+    const [name,setName] = useState("");
+    const [sliderVal, setSliderVal] = useState(0);
+    const updateName = (e)=>{
+        setName(e.target.value);
+    }
+    const handleSlider = (e,val) => {
+        setSliderVal(val)
+    }
+    return <div>
+        <h3>Name: {name}</h3>
+        <TextField id="outlined-basic" label="Name" variant="outlined" placeholder="Enter name"
+            value={name} onChange={updateName}/>
+        <TextField id="filled-basic" label="Name" variant="filled" placeholder="Enter name" value={name} onChange={updateName} />
+        <TextField id="standard-basic" label="Name" variant="standard" placeholder="name" value={name} onChange={updateName}/>
+
+        <br />
+        <h3>Volume: {sliderVal}</h3>
+        <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+            <VolumeDown />
+                <Slider aria-label="Volume" value={sliderVal} onChange={handleSlider} />
+            <VolumeUp />
+        </Stack>
+    </div>
+}
