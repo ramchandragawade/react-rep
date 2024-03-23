388c1b5cfe28121b2b1e542ed63010d957b3b90f
ramchandragawade <raajgawade39@gmail.com>
2024-03-24 00:37:18 +0530

    refactor

--- todoList/src/TodoItem.jsx
+++ todoList/src/TodoItem.jsx
@@ -1,31 +1,31 @@
 import { ListItem, IconButton, ListItemButton, ListItemIcon, Checkbox, ListItemText } from "@mui/material";
 import { Delete } from "@mui/icons-material";
 
-function TodoItem({item, updateCompleted, removeTodo}){
+function TodoItem({ item, updateCompleted, removeTodo }) {
     const labelId = `checkbox-list-label-${item.id}`;
     return (
         <ListItem
-        secondaryAction={
-            <IconButton edge="end" aria-label="comments" onClick={removeTodo}>
-                <Delete/>
-            </IconButton>
-        }
-        disablePadding
+            secondaryAction={
+                <IconButton edge="end" aria-label="comments" onClick={removeTodo}>
+                    <Delete />
+                </IconButton>
+            }
+            disablePadding
         >
-        <ListItemButton onClick={updateCompleted}>
-            <ListItemIcon>
-            <Checkbox
-                edge="start"
-                checked={item.completed}
-                tabIndex={-1}
-                disableRipple
-                inputProps={{ 'aria-labelledby': labelId }}
-            />
-            </ListItemIcon>
-            <ListItemText id={labelId} primary={item.text} />
-        </ListItemButton>
+            <ListItemButton onClick={updateCompleted}>
+                <ListItemIcon>
+                    <Checkbox
+                        edge="start"
+                        checked={item.completed}
+                        tabIndex={-1}
+                        disableRipple
+                        inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={item.text} />
+            </ListItemButton>
         </ListItem>
-    );  
+    );
 }
 
 export default TodoItem;
